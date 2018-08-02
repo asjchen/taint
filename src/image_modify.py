ec7d227573e85f582e031ee71036caf0ed53189a
Andy Chen <asjchen15@gmail.com>
2018-08-02 14:54:58 -0700

    Fixed displaying scaled image

--- src/image_modify.py
+++ src/image_modify.py
@@ -5,13 +5,16 @@
 import argparse
 import numpy as np
 from PIL import Image
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
 
 def color_to_grayscale(image_filename):
     gray_img = Image.open(image_filename).convert('LA')
     return gray_img
 
 def normalize_numpy(img_256_arr):
-    return img_256_arr / 255
+    return img_256_arr / 255.0
 
 def scale_and_crop_image(orig_img, final_height, final_width):
     if orig_img.size[0] * final_width != final_height * orig_img.size[1]:
@@ -25,16 +28,30 @@ def process_color_image(image_filename, final_height, final_width):
         raw_grayscale, final_height, final_width))[:, :, 0].transpose()
     return normalize_numpy(unnormed_grayscale)
 
+# Displays a NumPy array (with all elements <= 1) as a grayscale picture
+def display_normed_image(normed_np):
+    scaled_np = 256 * normed_np
+    plt.imshow(scaled_np)
+    plt.show()
+    plt.close()
+
 def main():
     parser = argparse.ArgumentParser(description=('Takes an image and a '
         'classifier model and outputs an adversarial example'))
     parser.add_argument('image_filename', 
         help='Filename of the (color) input image')
+    parser.add_argument('-d', '--display_image', action='store_true',
+        help='Displays the grayscale scaled image')
     args = parser.parse_args()
-    # arguments to take in the image, whether to display the image
     
-
-    # display it if asked
+    
+    # TODO: change 28 -- user should choose among several settings,
+    #  each w/ different JSON hyperparams
+    # Original input for the classifier, not for this program
+    orig_input = process_color_image(args.image_filename, 28, 28)
+    if args.display_image:
+        display_normed_image(orig_input)
+        
     # take in a model and identify the class (originally)
 
 if __name__ == '__main__':
